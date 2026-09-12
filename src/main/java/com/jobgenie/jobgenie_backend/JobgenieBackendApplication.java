6be9cbeaafd69a029b9828ad44ce288e21b0289c
theasthashukla212 <thasthashukla212@gmail.com>
2026-09-12 11:04:49 +0530

    Initial commit

---
+++ src/main/java/com/jobgenie/jobgenie_backend/JobgenieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobgenie.jobgenie_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobgenieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobgenieBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobgenie/jobgenie_backend/JobgenieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobgenie.jobgenie_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobgenieBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
